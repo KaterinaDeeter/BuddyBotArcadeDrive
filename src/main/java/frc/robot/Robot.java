6e1b3ba933c75b77278c2b20bed69435160cf8d4
Isaiah-Milkey <isaiah.milkey@pridetronics.com>
2022-01-22 09:48:47 -0700

    Change motor ids

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -25,10 +25,10 @@ public class Robot extends TimedRobot
   private DifferentialDrive m_myRobot;
 
   private Joystick m_leftStick;
-  //private Joystick m_rightStick; This was commented out to switch to arcade drive
+  private Joystick m_rightStick; 
 
-  private final MotorController m_leftMotor = new PWMSparkMax(1);
-  private final MotorController m_rightMotor = new PWMSparkMax(2);
+  private final MotorController m_leftMotor = new PWMSparkMax(0);
+  private final MotorController m_rightMotor = new PWMSparkMax(1);
   //private final MotorController m_leftMotor = new CANSparkMax(1, MotorType.kBrushed);
   //private final MotorController m_rightMotor = new CANSparkMax(2, MotorType.kBrushed);
 
@@ -46,15 +46,14 @@ public class Robot extends TimedRobot
     
     m_leftStick = new Joystick(0);
     
-    //m_rightStick = new Joystick(1); //This would require the other left joystick to move the robot. I'd like to figure out a system in which we can get drive on two controller joysticks.
+    m_rightStick = new Joystick(1); //This would require the other left joystick to move the robot. I'd like to figure out a system in which we can get drive on two controller joysticks.
   }
 
   @Override
   public void teleopPeriodic() 
   {
-    m_myRobot.tankDrive(m_leftStick.getY(), m_leftStick.getX()); //by altering this code, it became arcade drive rather than tank (initially value was m_rightStick.getY())
+    m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY()); //by altering this code, it became arcade drive rather than tank (initially value was m_rightStick.getY())
     SmartDashboard.putNumber("Joystick x value", m_leftStick.getX()); //Test code for 01/20/2022. We're attempting to figure out how to 
     SmartDashboard.putNumber("Joystick y value", m_leftStick.getY()); //get values to display on Shuffleboard
   }
 }
-
